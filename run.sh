9fccc250c54f85fba7f112c65d4960a9f8c72cf2
N-Lefevre-UDEM <nathanael.lefevre@umontreal.ca>
2022-02-28 18:12:10 +0100

    ajout de l'affichage d'un lien pour tester la génération de phrase

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 echo 'http://0.0.0.0:8080/?method=words&length=100'
 echo 'http://0.0.0.0:8080/?method=characters&length=100&characterList=abcdefghijklmnopqrstuvwxyz&ban=true'
+echo 'http://0.0.0.0:8080/?method=sentence&length=10'
 
 gunicorn --bind 0.0.0.0:8080 app:app
