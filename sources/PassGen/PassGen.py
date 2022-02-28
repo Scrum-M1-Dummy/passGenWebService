1613e38cbb3501510fb2095e4ab12c7a8b350604
Audran <bera1109@usherbrooke.ca>
2022-02-28 11:17:03 -0500

    add: method include

--- sources/PassGen/PassGen.py
+++ sources/PassGen/PassGen.py
@@ -1,5 +1,8 @@
 import string
 import secrets
+import random
+
+from sources.keys import *
 from sources.Data.DataGetter import DataGetter
 
 
@@ -16,14 +19,16 @@ class PassGen:
             list of the characters to USE for the password
           """
         # TODO : handle the "must" value for character_selection_method aka ban
-        if character_selection_method == "ban":
-            whole_alphabet = string.digits + string.ascii_letters
-            restricted_alphabet = ""
+        restricted_alphabet = ""
+        whole_alphabet = string.digits + string.ascii_letters
+        if character_selection_method == METHOD_BAN:
             for i in whole_alphabet:
                 if i not in character_list:
                     restricted_alphabet += i
-        else:
+        elif character_selection_method == METHOD_ONLY:
             restricted_alphabet = character_list
+        else:
+            return whole_alphabet
         return restricted_alphabet
 
     @classmethod
@@ -61,8 +66,15 @@ class PassGen:
         print(character_list)
         print(character_selection_method)
         alphabet = PassGen.get_alphabet_character_choice(character_list, character_selection_method)
-        return ''.join(secrets.choice(alphabet) for _ in range(length))
+        if character_selection_method == METHOD_INCLUDE:
+            mdp=''.join(secrets.choice(alphabet) for _ in range(length-len(character_list)))
+            for i in range(len(character_list)):
+                r=random.randint(0,len(mdp))
+                mdp=mdp[:r]+character_list[i]+mdp[r:]
+            return mdp
+        else:
+            return ''.join(secrets.choice(alphabet) for _ in range(length))
 
 
 if __name__ == "__main__":
-    print(PassGen.get_password_character_choice(length=10, character_list="abcd", character_selection_method="ban"))
+    print(PassGen.get_password_character_choice(length=6, character_list="abcd", character_selection_method="include"))
