b96f9ff6ebb91beaa2c4c85aa7a07db45851bd55
N-Lefevre-UDEM <nathanael.lefevre@umontreal.ca>
2022-03-07 09:46:26 +0100

    gestion de l'anglais version lente

--- sources/PassGen/Models/PhraseExpert.py
+++ sources/PassGen/Models/PhraseExpert.py
@@ -1,21 +1,48 @@
 from sources.Data.DataGetter import DataGetter
 import numpy as np
+import json
+import os
 
 
 class PhraseExpert:
     def gen_co_occurence_matrice(self):
+        print("nar")
         self.occ_matrix = np.zeros(shape=(self.nb_unique_words, self.nb_unique_words))
         for i in range(self.nb_corpus_words - 1):
+            print(i, "/", self.nb_corpus_words - 1)
             if self.corpus[i] != '' and self.corpus[i + 1] != '':
                 self.occ_matrix[self.unique_words.index(self.corpus[i])][self.unique_words.index(self.corpus[i + 1])] += 1
 
-    def __init__(self, corpus):
-        self.corpus = corpus
-        self.nb_corpus_words = len(corpus)
-        self.unique_words = list(set(corpus))
-        self.unique_words.remove('')
-        self.nb_unique_words = len(self.unique_words)
-        self.gen_co_occurence_matrice()
+    def __init__(self, corpus_getter):
+        if os.path.exists(corpus_getter.__name__ + ".json"):
+            with open(corpus_getter.__name__ + ".json", 'r') as file:
+                print("loading from file")
+                phrase_gen_obj = json.load(file)
+                self.nb_corpus_words = phrase_gen_obj["nb_corpus_words"]
+                self.unique_words = phrase_gen_obj["unique_words"]
+
+                self.occ_matrix = np.loadtxt(corpus_getter.__name__ + ".np")
+        else:
+            corpus = corpus_getter()
+            self.corpus = corpus
+            self.nb_corpus_words = len(corpus)
+            self.unique_words = list(set(corpus))
+            # noinspection PyBroadException
+            try:
+                self.unique_words.remove('')
+            except Exception as e:
+                pass
+            self.nb_unique_words = len(self.unique_words)
+            self.gen_co_occurence_matrice()
+
+            phrase_gen_obj = {"nb_corpus_words": self.nb_corpus_words,
+                              "unique_words": self.unique_words
+                              }
+
+            with open(corpus_getter.__name__ + ".json", 'w') as outfile:
+                outfile.write(json.dumps(phrase_gen_obj))
+
+            np.savetxt(corpus_getter.__name__ + ".np", self.occ_matrix)
 
     def pick_random_word(self):
         return self.unique_words[np.random.randint(0, len(self.unique_words))]
@@ -36,5 +63,8 @@ class PhraseExpert:
 
 
 if __name__ == "__main__":
-    pe = PhraseExpert(DataGetter.get_apple_text_words())
+    #pe = PhraseExpert(DataGetter.get_apple_text_words())
+    print("hi")
+    pe = PhraseExpert(DataGetter.get_ang_sentences)
+    print("coucou")
     print(pe.gen_phrase(10))
